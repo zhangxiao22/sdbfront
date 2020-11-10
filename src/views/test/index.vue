c032c3b2cace3e5671eb62125bf25e8c645c9fce
sdebank <anyishuo@126.com>
2020-11-10 22:06:06 +0800

    黑名单修改

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container">
-    <div id="canvas"
-         style="width:300px;height:500px;"
-         class="chart" />
+    <div class="block">
+      {{ value1 }} {{ typeof value1 }}
+      <span class="demonstration">默认</span>
+      <el-date-picker v-model="value1"
+                      type="daterange"
+                      range-separator="至"
+                      start-placeholder="开始日期"
+                      end-placeholder="结束日期" />
+    </div>
   </div>
 </template>
 
@@ -15,6 +21,7 @@ export default {
 
   data() {
     return {
+      value1: '',
       loading: false,
       tableData: [{
         address: '1111'
@@ -33,34 +40,12 @@ export default {
 
   watch: {},
   created() {
-    this.getList()
   },
   mounted() {
-    const data = [
-      { stage: '简历筛选', number: 253 },
-      { stage: '初试人数', number: 151 },
-      { stage: '复试人数', number: 113 },
-      { stage: '录取人数', number: 87 },
-      { stage: '入职人数', number: 59 }
-    ]
 
-    const funnelPlot = new Funnel('canvas', {
-      data: data,
-      xField: 'stage',
-      yField: 'number',
-      dynamicHeight: true,
-      legend: false
-    })
-
-    funnelPlot.render()
   },
   methods: {
-    rowKey(row) {
-      return row.address
-    },
-    getList() {
 
-    }
   }
 }
 </script>
