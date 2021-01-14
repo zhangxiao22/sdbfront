e8d29447a8f82e85ff44ed77f888b5108a3421ec
sdebank <anyishuo@126.com>
2021-01-14 17:11:14 +0800

    ok

--- src/router/permission.js
+++ src/router/permission.js
@@ -11,7 +11,7 @@ NProgress.configure({ showSpinner: false }) // NProgress Configuration
 const whiteList = ['/401', '/404'] // no redirect whitelist
 
 router.beforeEach(async (to, from, next) => {
-  // console.log(to)
+  console.log(to.path)
   // // start progress bar
   NProgress.start()
 
@@ -21,25 +21,31 @@ router.beforeEach(async (to, from, next) => {
   // 获取用户的权限(角色)
   const hasRoles = store.getters.roles
   // console.log('hasRoles>>>>>>>>>>>>>>>>>>>>>>>>', hasRoles)
-  if (hasRoles) {
+  const isIncludeWhiteList = whiteList.includes(to.path)
+  // console.log('isIncludeWhiteList>>>>>>>>>>>>>>>>>>>>>>>', isIncludeWhiteList)
+  if (isIncludeWhiteList || hasRoles) {
     next()
   } else {
-    const data = await store.dispatch('user/getInfo')
-    // console.log('process.env.NODE_ENV>>>>>>>>>>>>>>', process.env.NODE_ENV)
-    let roles
-    if (process.env.NODE_ENV === 'development') {
-      roles = 'admin'
-      // roles = data.permissionPack.label
-      store.commit('user/SET_ROLES', roles)
-    } else {
-      roles = data.permissionPack.label
+    try {
+      const data = await store.dispatch('user/getInfo')
+      // console.log('process.env.NODE_ENV>>>>>>>>>>>>>>', process.env.NODE_ENV)
+      let roles
+      if (process.env.NODE_ENV === 'development') {
+        // roles = 'admin'
+        roles = data?.permissionPack.label
+        store.commit('user/SET_ROLES', roles)
+      } else {
+        roles = data?.permissionPack.label
+      }
+      // console.log('roles>>>>>>>>>>>>>>>>>>>>>>>', roles)
+      const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
+      // console.log('accessRoutes>>>>>>>>>>>>>>>>>>>>>>>>>', accessRoutes)
+      router.addRoutes(accessRoutes)
+      // next()
+      next({ ...to, replace: true })
+    } catch (e) {
+      console.log(e)
     }
-    // console.log('roles>>>>>>>>>>>>>>>>>>>>>>>', roles)
-    const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
-    // console.log('accessRoutes>>>>>>>>>>>>>>>>>>>>>>>>>', accessRoutes)
-    router.addRoutes(accessRoutes)
-    // next()
-    next({ ...to, replace: true })
   }
   NProgress.done()
 })
