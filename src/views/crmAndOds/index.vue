94e8086d69bff04927186831f0086455aaa0253c
abc <>
2021-03-09 10:16:10 +0800

    全部下载改为下载

--- src/views/crmAndOds/index.vue
+++ src/views/crmAndOds/index.vue
@@ -38,13 +38,18 @@
         </el-form>
       </template>
       <template v-slot:main-buttons>
-        <el-button class="button"
-                   icon="el-icon-download"
-                   type="success"
-                   plain
-                   @click="downloadAll">
-          全部下载
-        </el-button>
+        <el-tooltip class="item"
+                    effect="dark"
+                    content="下载当前搜索结果"
+                    placement="top">
+          <el-button class="button"
+                     icon="el-icon-download"
+                     type="success"
+                     plain
+                     @click="downloadAll">
+            下载
+          </el-button>
+        </el-tooltip>
       </template>
     </shun-table>
   </div>
@@ -138,7 +143,10 @@ export default {
       })
     },
     downloadAll() {
-      downloadFile('/clue/downloadRecord', {})
+      const data = {
+        dateRange: this.searchForm.dateRange.join(',')
+      }
+      downloadFile('/clue/downloadRecord', data)
     }
   }
 }
