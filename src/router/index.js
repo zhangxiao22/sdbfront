9ceeef4e884ec1e3c8503575d1e8bbfa585813a0
abc <>
2021-03-10 15:42:46 +0800

    用例库开放给审批钢

--- src/router/index.js
+++ src/router/index.js
@@ -41,7 +41,7 @@ export const asyncRoutes = [
         name: 'UseCase',
         component: () => import('@/views/useCase/index'),
         meta: {
-          title: '用例库', icon: 'useCase', roles: ['用例管理', '线索统筹']
+          title: '用例库', icon: 'useCase', roles: ['用例管理', '线索统筹', '领导审批']
         }
       },
       {
