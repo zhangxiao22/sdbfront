d91824bb961e971db0414440d612c64d26736d8b
abc <>
2021-04-01 16:43:16 +0800

    fixed

--- src/views/config/eventPriority.vue
+++ src/views/config/eventPriority.vue
@@ -47,8 +47,7 @@
       <el-table-column prop="smsWeekClueLimit"
                        show-overflow-tooltip
                        label="每周线索分配上限（短信）" />
-      <el-table-column fixed="right"
-                       label="操作"
+      <el-table-column label="操作"
                        width="100">
         <template slot-scope="scope">
           <div class="operate-btns">
--- src/views/config/useCasePriority.vue
+++ src/views/config/useCasePriority.vue
@@ -26,8 +26,7 @@
       <el-table-column prop="description"
                        show-overflow-tooltip
                        label="描述" />
-      <el-table-column fixed="right"
-                       label="操作"
+      <el-table-column label="操作"
                        width="100">
         <template slot-scope="scope">
           <div class="operate-btns">
