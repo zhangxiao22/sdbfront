7915c0ef86394bed23b512532a0dd44ddbf7e9ac
Shay <shay7sev@gmail.com>
2021-04-13 15:43:46 +0800

    1

--- src/views/config/globalOutletDistribute.vue
+++ src/views/config/globalOutletDistribute.vue
@@ -4,77 +4,153 @@
     <el-form ref="regFormRef"
              :model="form"
              class="main-form">
-      <el-form-item class="form-item">
-        <div class="form-header">
-          <el-form-item required
-                        class="item"
-                        label="网点："
-                        style="flex:1;" />
-          <el-form-item required
-                        class="item"
-                        label="比例："
-                        style="width:200px;" />
-          <el-form-item required
-                        class="item"
-                        label="有效期："
-                        style="width:300px;" />
-        </div>
-        <div v-for="(outletItem,i) of form.outlets"
-             :key="i"
-             class="block-item">
-          <el-form-item :prop="'outlets.'+i+'.item'"
-                        :rules="{
-                          required: true, message: '请选择网点', trigger: 'change'
-                        }">
-            <el-select v-model="outletItem.item"
-                       filterable
-                       placeholder="请选择网点"
-                       class="item-input"
-                       @change="handleSelectOutlet($event,outletItem)">
-              <el-option v-for="optItem of outletOpt"
-                         :key="optItem.value"
-                         :disabled="optItem.disabled"
-                         :label="optItem.label"
-                         :value="optItem.value" />
-            </el-select>
-          </el-form-item>
-          <el-form-item :prop="'outlets.'+i+'.value'"
-                        :rules="{
-                          required: true, message: '请输入分配比例', trigger: 'change'
-                        }">
-            <el-input-number v-model="outletItem.value"
-                             style="width:200px;"
-                             :disabled="!outletItem.item"
-                             :min="0"
-                             :max="200"
-                             :step="10"
-                             controls-position="right"
-                             placeholder="请输入分配比例"
-                             class="item-input number-input" />
-            <div class="unit">%</div>
-          </el-form-item>
-          <el-form-item :prop="'outlets.'+i+'.dateRange'"
-                        :rules="[{
-                          required: true, message: '请输入有效期', trigger: 'blur'
-                        }]">
-            <el-date-picker v-model="outletItem.dateRange"
-                            style="width:300px;"
-                            value-format="yyyy-MM-dd"
-                            class="item-date"
-                            type="daterange"
-                            :picker-options="pickerOptions"
-                            range-separator="至"
-                            start-placeholder="开始日期"
-                            end-placeholder="结束日期" />
-          </el-form-item>
-          <i class="el-icon-delete delete"
-             @click="delOutletItem(i)" />
-        </div>
-        <el-button v-if="form.outlets.length < outletOpt.length"
-                   class="add"
-                   icon="el-icon-plus"
-                   @click="addOutlet" />
-      </el-form-item>
+      <div class="box">
+        <el-form-item class="form-item">
+          <div class="form-header">
+            <el-form-item required
+                          class="item"
+                          label="网点："
+                          style="flex:1;" />
+            <el-form-item required
+                          class="item"
+                          label="比例："
+                          style="width:200px;" />
+            <el-form-item required
+                          class="item"
+                          label="有效期："
+                          style="width:300px;" />
+          </div>
+          <div v-for="(globalOutletItem,i) of form.globalOutlets"
+               :key="i"
+               class="block-item">
+            <!-- <el-form-item :prop="'globalOutlets.'+i+'.globalItem'"
+                          :rules="{
+                            required: true, message: '请选择网点', trigger: 'change'
+                          }">
+              <el-select v-model="globalOutletItem.globalItem"
+                         filterable
+                         placeholder="请选择网点"
+                         class="item-input">
+                <el-option v-for="item of globalOpt"
+                           :key="item.value"
+                           :label="item.label"
+                           :value="item.value" />
+              </el-select>
+            </el-form-item> -->
+            <el-form-item style="flex:1;">
+              全局
+            </el-form-item>
+            <el-form-item :prop="'globalOutlets.'+i+'.globalValue'"
+                          :rules="{
+                            required: true, message: '请输入分配比例', trigger: 'change'
+                          }">
+              <el-input-number v-model="globalOutletItem.globalValue"
+                               style="width:200px;"
+                               :disabled="!globalOutletItem.globalItem"
+                               :min="0"
+                               :max="200"
+                               :step="10"
+                               controls-position="right"
+                               placeholder="请输入分配比例"
+                               class="item-input number-input" />
+              <div class="unit">%</div>
+            </el-form-item>
+            <el-form-item :prop="'globalOutlets.'+i+'.globalDateRange'"
+                          :rules="[{
+                            required: true, message: '请输入有效期', trigger: 'blur'
+                          }]">
+              <el-date-picker v-model="globalOutletItem.globalDateRange"
+                              style="width:300px;"
+                              value-format="yyyy-MM-dd"
+                              class="item-date"
+                              type="daterange"
+                              :picker-options="pickerOptions"
+                              range-separator="至"
+                              start-placeholder="开始日期"
+                              end-placeholder="结束日期" />
+            </el-form-item>
+            <!-- <i class="el-icon-delete delete"
+               @click="delOutletItem(i)" /> -->
+          </div>
+          <!-- <el-button v-if="form.outlets.length < outletOpt.length"
+                     class="add"
+                     icon="el-icon-plus"
+                     @click="addOutlet" /> -->
+        </el-form-item>
+      </div>
+      <div class="box">
+        <el-form-item class="form-item">
+          <div class="form-header">
+            <el-form-item required
+                          class="item"
+                          label="网点："
+                          style="flex:1;" />
+            <el-form-item required
+                          class="item"
+                          label="比例："
+                          style="width:200px;" />
+            <el-form-item required
+                          class="item"
+                          label="有效期："
+                          style="width:300px;" />
+          </div>
+          <div v-for="(outletItem,i) of form.outlets"
+               :key="i"
+               class="block-item">
+            <el-form-item :prop="'outlets.'+i+'.item'"
+                          :rules="{
+                            required: true, message: '请选择网点', trigger: 'change'
+                          }">
+              <el-select v-model="outletItem.item"
+                         filterable
+                         placeholder="请选择网点"
+                         class="item-input"
+                         @change="handleSelectOutlet($event,outletItem)">
+                <el-option v-for="optItem of outletOpt"
+                           :key="optItem.value"
+                           :disabled="optItem.disabled"
+                           :label="optItem.label"
+                           :value="optItem.value" />
+              </el-select>
+            </el-form-item>
+            <el-form-item :prop="'outlets.'+i+'.value'"
+                          :rules="{
+                            required: true, message: '请输入分配比例', trigger: 'change'
+                          }">
+              <el-input-number v-model="outletItem.value"
+                               style="width:200px;"
+                               :disabled="!outletItem.item"
+                               :min="0"
+                               :max="200"
+                               :step="10"
+                               controls-position="right"
+                               placeholder="请输入分配比例"
+                               class="item-input number-input" />
+              <div class="unit">%</div>
+            </el-form-item>
+            <el-form-item :prop="'outlets.'+i+'.dateRange'"
+                          :rules="[{
+                            required: true, message: '请输入有效期', trigger: 'blur'
+                          }]">
+              <el-date-picker v-model="outletItem.dateRange"
+                              style="width:300px;"
+                              value-format="yyyy-MM-dd"
+                              class="item-date"
+                              type="daterange"
+                              :picker-options="pickerOptions"
+                              range-separator="至"
+                              start-placeholder="开始日期"
+                              end-placeholder="结束日期" />
+            </el-form-item>
+            <i class="el-icon-delete delete"
+               @click="delOutletItem(i)" />
+          </div>
+          <el-button v-if="form.outlets.length < outletOpt.length"
+                     class="add"
+                     icon="el-icon-plus"
+                     @click="addOutlet" />
+        </el-form-item>
+      </div>
       <el-form-item>
         <el-button icon="el-icon-document"
                    type="primary"
@@ -117,12 +193,37 @@ export default {
             value: '',
             dateRange: []
           }
+        ],
+        globalOutlets: [
+          {
+            globalItem: 999,
+            globalValue: '100',
+            globalDateRange: []
+          }
         ]
       },
+      globalOpt: [{
+        label: '全局',
+        value: 999
+      }],
       outletOpt: []
     }
   },
   computed: {
+    // 获取全局分配比例数据
+    getGlobalData() {
+      const data = {}
+      data.outletAllotList = this.form.globalOutlets.map(n => {
+        return {
+          outletId: n.globalItem,
+          proportion: n.globalValue,
+          startDate: n.globalDateRange[0],
+          endDate: n.globalDateRange[1]
+        }
+      })
+      data.category = 3
+      return data
+    },
     // 获取数据
     getData() {
       const data = {}
@@ -178,22 +279,48 @@ export default {
         }
         this.resetOutLet()
       })
+      getOutletAllot({ category: 3 }).then(res => {
+        // 获取网点加分配比例值
+        if (res.data.length) {
+          this.form.globalOutlets = res.data.map(item => {
+            return {
+              globalDateRange: item.startDate ? [item.startDate, item.endDate] : [],
+              globalItem: item.outletId,
+              globalValue: item.proportion
+            }
+          })
+        }
+      })
+    },
+    saveGlobal() {
+      insertOutletAllotBatch(this.getGlobalData).then(res => {
+        if (res.code === 200) {
+          this.$message({
+            message: '保存全局数据成功',
+            type: 'success',
+            duration: '3000'
+          })
+        }
+      })
+    },
+    saveOutlet() {
+      insertOutletAllotBatch(this.getData).then(res => {
+        if (res.code === 200) {
+          this.$message({
+            message: '保存网点数据成功',
+            type: 'success',
+            duration: '3000'
+          })
+        }
+      })
     },
     save() {
       this.$refs['regFormRef'].validate((valid) => {
         if (valid) {
           this.$emit('update:loading', true)
-          insertOutletAllotBatch(this.getData).then(res => {
-            if (res.code === 200) {
-              this.$message({
-                message: '保存成功',
-                type: 'success',
-                duration: '3000'
-              })
-            }
-          }).finally(() => {
-            this.$emit('update:loading', false)
-          })
+          this.saveGlobal()
+          this.saveOutlet()
+          this.$emit('update:loading', false)
         }
       })
     },
@@ -257,65 +384,75 @@ export default {
   .main-form {
     margin: 20px auto;
     width: 715px;
-    .form-item {
-      .form-header {
-        display: flex;
-        .item {
-          margin-right: 10px;
-          margin-bottom: 0;
-          // border: 1px solid;
-          &:last-of-type {
-            margin-right: 0;
-          }
-        }
+    .box {
+      border-bottom: 1px solid #eee;
+      padding-top: 18px;
+      &:last-of-type {
+        border-bottom: none;
       }
-      .block-item {
-        display: flex;
-        position: relative;
-        margin-bottom: 2px;
-        .el-form-item {
-          position: relative;
-          margin-right: 10px;
-          &:last-of-type {
-            margin-right: 0;
-          }
-          .item-input {
-            width: 100%;
-            position: relative;
-            &.number-input {
-              ::v-deep .el-input__inner {
-                padding-right: 80px;
-                text-align: left;
-              }
+      &:first-of-type {
+        padding-top: 0;
+      }
+      .form-item {
+        .form-header {
+          display: flex;
+          .item {
+            margin-right: 10px;
+            margin-bottom: 0;
+            // border: 1px solid;
+            &:last-of-type {
+              margin-right: 0;
             }
           }
-          .unit {
-            position: absolute;
-            bottom: 0;
-            right: 42px;
-          }
         }
+        .block-item {
+          display: flex;
+          position: relative;
+          margin-bottom: 2px;
+          .el-form-item {
+            position: relative;
+            margin-right: 10px;
+            &:last-of-type {
+              margin-right: 0;
+            }
+            .item-input {
+              width: 100%;
+              position: relative;
+              &.number-input {
+                ::v-deep .el-input__inner {
+                  padding-right: 80px;
+                  text-align: left;
+                }
+              }
+            }
+            .unit {
+              position: absolute;
+              bottom: 0;
+              right: 42px;
+            }
+          }
 
-        .delete {
-          color: $red;
-          display: inline-block;
-          width: 20px;
-          cursor: pointer;
-          height: 32px;
-          margin-left: 10px;
-          font-size: 18px;
-          line-height: 32px;
-          position: absolute;
-          right: -30px;
-          top: 0;
-          &:hover {
-            opacity: 0.8;
+          .delete {
+            color: $red;
+            display: inline-block;
+            width: 20px;
+            cursor: pointer;
+            height: 32px;
+            margin-left: 10px;
+            font-size: 18px;
+            line-height: 32px;
+            position: absolute;
+            right: -30px;
+            top: 0;
+            &:hover {
+              opacity: 0.8;
+            }
           }
         }
-      }
-      .add {
-        width: 100%;
-        border-style: dashed;
+        .add {
+          width: 100%;
+          border-style: dashed;
+        }
       }
     }
   }
--- src/views/config/index.vue
+++ src/views/config/index.vue
@@ -52,7 +52,7 @@ export default {
   },
   data() {
     return {
-      tabIndex: '0',
+      tabIndex: '2',
       tabList: [{
         label: '理顾指派',
         component: 'Appoint',
