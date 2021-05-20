c36605f2bcfa89ed7807fc6638ca38d187530146
sdebank <anyishuo@126.com>
2021-05-20 14:11:43 +0800

    修改分配规则权限

--- src/views/config/index.vue
+++ src/views/config/index.vue
@@ -58,7 +58,7 @@ export default {
   },
   data() {
     return {
-      tabIndex: '1',
+      tabIndex: '0',
       tabList: [{
         label: '理财顾问指派',
         component: 'Appoint',
@@ -130,7 +130,7 @@ export default {
         label: '分配规则',
         component: 'Rule',
         loading: false,
-        roles: ['业务管理', '用例管理']
+        roles: ['线索统筹']
       },
       {
         label: '防打扰名单（CRM）',
