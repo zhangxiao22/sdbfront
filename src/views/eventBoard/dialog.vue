52eed2e119ea10e0ce853fb2ca0066952b98fb51
zhangheng7sev@gmail.com <zhangheng7sev@gmail.com>
2021-06-18 14:38:48 +0800

    新增

---
+++ src/views/eventBoard/dialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="edit-container">
+    <el-dialog title="编辑事件"
+               :visible="visible">
+      123
+    </el-dialog>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default() {
+        return false
+      }
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
