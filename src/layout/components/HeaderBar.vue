e311eda4a88716b5a8b4f1dbe7c2dc1acc7b6a7c
Shay <shay7sev@gmail.com>
2021-07-06 09:26:13 +0800

    路由跳转注释

--- src/layout/components/HeaderBar.vue
+++ src/layout/components/HeaderBar.vue
@@ -76,7 +76,7 @@ export default {
     },
     async logout() {
       await this.$store.dispatch('user/logout')
-      this.$router.push(`/start?redirect=${this.$route.fullPath}`)
+      // this.$router.push(`/start?redirect=${this.$route.fullPath}`)
     }
   }
 }
