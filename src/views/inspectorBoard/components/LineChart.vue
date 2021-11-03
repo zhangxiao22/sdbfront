d6b514ffe4dc2033300201b13f33bb982b5f8746
jack <jackdu2013@qq.com>
2021-11-03 14:48:33 +0800

    画线很难

--- src/views/inspectorBoard/components/LineChart.vue
+++ src/views/inspectorBoard/components/LineChart.vue
@@ -25,7 +25,7 @@ export default {
   },
   created() {
     this.legendItem = ''
-    this.isAnnotationShown = false
+    this.isShow = false
   },
   mounted() {
     this.render()
@@ -60,16 +60,11 @@ export default {
           formatter: n => `${Math.floor(n * 100)}%`
         }
       })
-      this.chart
-        .line()
-        .position('label*value')
-        .color('category')
-        .shape('smooth')
-      this.chart
-        .point()
-        .position('label*value')
-        .color('category')
-        .shape('circle')
+      this.line = this.chart.line().position('label*value').color('category').shape('smooth')
+      this.line.label('value')
+      this.line.label(false)
+
+      this.chart.point().position('label*value').color('category').shape('circle')
       this.chart.interaction('legend-filter', {
         showEnable: [
           { trigger: 'legend-item:mouseenter', action: 'cursor:pointer' },
@@ -80,15 +75,14 @@ export default {
           { trigger: 'legend-item:click', action: 'data-filter:filter' }
         ]
       })
-      this.chart.on('legend-item:click', (e) => {
+      this.chart.render()
+      this.chart.on('legend:click', (e) => {
+        const legendItemsList = e.gEvent.delegateObject.legend.cfg.items
+        const checkedNumber = legendItemsList.filter(n => !n.unchecked).length
         this.chart.annotation().clear(true)
-        const legendItem = e.gEvent.target.cfg.delegateObject.item.id
-        const filteredData = this.chart.getData()
-        if (filteredData.length <= 0) {
-          return
-        }
-        const yMax = Math.max(...filteredData.map(n => n.value))
-        const annotate = () => {
+        if (checkedNumber === 1) {
+          const filteredData = this.chart.getData()
+          const yMax = Math.max(...filteredData.map(n => n.value))
           this.chart.annotation().line({
             start: ['min', yMax],
             end: ['max', yMax],
@@ -106,22 +100,77 @@ export default {
               offsetY: -5
             }
           })
-        }
-        if (this.legendItem !== legendItem) {
-          this.legendItem = legendItem
-          this.isAnnotationShown = true
-          annotate()
+          this.line.label('value')
         } else {
-          if (this.isAnnotationShown) {
-            this.isAnnotationShown = false
-          } else {
-            this.isAnnotationShown = true
-            annotate()
-          }
+          console.log('all')
+          this.line.label(false)
         }
-        this.chart.changeData(this.data)
+        console.log(this.line.getAttribute('label'))
+        this.chart.render(true)
+        // setTimeout(() => {
+        //   this.line.label(false)
+        //   this.chart.render(true)
+        // }, 3000)
+
+        // this.chart.annotation().clear(true)
+        // this.line.label(false)
+        // const legendItem = e.gEvent.target.cfg.delegateObject.item.id
+        // const filteredData = this.chart.getData()
+        // if (filteredData.length <= 0) {
+        //   return
+        // }
+        // const yMax = Math.max(...filteredData.map(n => n.value))
+        // const annotate = () => {
+        //   this.chart.annotation().line({
+        //     start: ['min', yMax],
+        //     end: ['max', yMax],
+        //     style: {
+        //       stroke: '#ff4d4f',
+        //       lineWidth: 1,
+        //       lineDash: [3, 3]
+        //     }, text: {
+        //       position: 'end',
+        //       style: {
+        //         fill: 'grey',
+        //         fontWeight: 'normal'
+        //       },
+        //       content: percentFormatter(null, null, yMax),
+        //       offsetY: -5
+        //     }
+        //   })
+        // }
+        // if (this.legendItem !== legendItem) {
+        //   this.legendItem = legendItem
+        //   this.isShow = true
+        //   annotate()
+        //   this.line.label('value', value => ({
+        //     content: percentFormatter(null, null, value),
+        //     layout: {
+        //       type: 'overlap'
+        //     },
+        //     style: {
+        //       fill: 'grey'
+        //     }
+        //   }))
+        // } else {
+        //   if (this.isShow) {
+        //     this.isShow = false
+        //   } else {
+        //     this.isShow = true
+        //     annotate()
+        //     this.line.label('value', value => ({
+        //       content: percentFormatter(null, null, value),
+        //       layout: {
+        //         type: 'overlap'
+        //       },
+        //       style: {
+        //         fill: 'grey'
+        //       }
+        //     }))
+        //   }
+        // }
+        // this.chart.changeData(this.data)
       })
-      this.chart.render()
     }
   }
 }
